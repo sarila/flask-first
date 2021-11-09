f03cdcf305010c56cd75a6a2fb5ee10d8df77bdd
Sarila <ngakhusisarila@gmail.com>
2021-11-09 16:58:30 +0545

    Relationship Established between user and items

--- market/models.py
+++ market/models.py
@@ -1,12 +1,23 @@
 from market import db
 
+# User table
+class User(db.Model):
+	id = db.Column(db.Integer(), primary_key=True)
+	username = db.Column(db.String(length=30), nullable=False, unique=True)
+	email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+	password_hash = db.Column(db.String(length=60), nullable=False)
+	budget = db.Column(db.Integer(), nullable=False, default=1000)
+	# Relationship between user and items defined
+	items = db.relationship('Item', backref="owned_user", lazy=True)
+
 # Model of a table
 class Item(db.Model):
 	id = db.Column(db.Integer(), primary_key=True)
 	name = db.Column(db.String(length=20), nullable=True, unique=True)
 	price = db.Column(db.Integer(), nullable=False, unique=True)
 	barcode = db.Column(db.String(length=12), nullable=False, unique=True)
-	description = db.Column(db.String(length=2000) )
+	description = db.Column(db.String(length=2000), nullable=False)
+	owner = db.Column(db.Integer(), db.ForeignKey('user_id'))
 
 	def __repr__(self):
 		return f'Item {self.name}'
