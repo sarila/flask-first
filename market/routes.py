2bf40b04ce6e10b8507a1f6a03e1b32a6bda04e9
Sarila <ngakhusisarila@gmail.com>
2021-11-12 22:38:49 +0545

    Flask form validation done with messages

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for, flash
+from market.models import Item, User
 from market.forms import RegisterForm
+from market import db
 
 # route decorater
 @app.route('/')
@@ -23,7 +24,19 @@ def upload():
 	return render_template('upload.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST']) #to handle different HTTP request
 def register_page():
 	form = RegisterForm()
+	# if form is submited do this
+	if form.validate_on_submit():
+		user_to_create = User(username=form.username.data,
+			email_address=form.email_address.data,
+			password_hash=form.password1.data)
+		db.session.add(user_to_create)
+		db.session.commit()
+		return redirect(url_for('market'))
+	if form.errors != {}: #If no errors from validations
+		for err_msg in form.errors.values():
+			flash(f'There was error creating user: {err_msg}', category='danger')
+
 	return render_template('register.html', form=form)
